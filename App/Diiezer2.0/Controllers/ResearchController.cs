aa25412218e26a9fb8b07baae0742384a138f1a2
Skyndia <balleuxbenjamin@gmail.com>
2016-05-13 12:30:33 +0200

    ajout de la vue pour la recherche, interface de recherche + controller de base

---
+++ App/Diiezer2.0/Controllers/ResearchController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Diiezer2._0.Controllers
+{
+    public class ResearchController : Controller
+    {
+        // GET: Research
+        public ActionResult Begin()
+        {
+
+
+            return View();
+        }
+    }
+}
