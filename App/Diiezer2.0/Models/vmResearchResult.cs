3ca1daf029c9745fa45c01c76c2e7d8072b13c5f
benjamin balleux <benjamin balleux>
2016-05-14 11:34:24 +0200

    vm pour les recherche

---
+++ App/Diiezer2.0/Models/vmResearchResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Diiezer2._0.Models
+{
+    public class vmResearchResult
+    {
+        public List<Artiste> artistes { get; set; }
+        public List<Album> albums { get; set; }
+        public List<Chanson> chansons { get; set; }
+    }
+}
+
