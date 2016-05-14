bad5a275f968144569f4c0b72b68a19c9538042c
Sensou <micka-el@wanadoo.fr>
2016-05-14 15:37:04 +0200

    tentative de mise en place de methodes post notes

---
+++ App/Diiezer2.0/Controllers/AchatController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Diiezer2._0.Models;
+
+namespace Diiezer2._0.Controllers
+{
+    public class AchatController : Controller
+    {
+        private DiiezerDBEntities db = new DiiezerDBEntities();
+
+        // GET: Achat
+        public ActionResult Index()
+        {
+            var achat = db.Achat.Include(a => a.Chanson1);
+            return View(achat.ToList());
+        }
+
+        // GET: Achat/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Achat achat = db.Achat.Find(id);
+            if (achat == null)
+            {
+                return HttpNotFound();
+            }
+            return View(achat);
+        }
+
+        // GET: Achat/Create
+        public ActionResult Create()
+        {
+            ViewBag.Chanson = new SelectList(db.Chanson, "Id", "Titre");
+            return View();
+        }
+
+        // POST: Achat/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Chanson,Utilisateur")] Achat achat)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Achat.Add(achat);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Chanson = new SelectList(db.Chanson, "Id", "Titre", achat.Chanson);
+            return View(achat);
+        }
+
+        // GET: Achat/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Achat achat = db.Achat.Find(id);
+            if (achat == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Chanson = new SelectList(db.Chanson, "Id", "Titre", achat.Chanson);
+            return View(achat);
+        }
+
+        // POST: Achat/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Chanson,Utilisateur")] Achat achat)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(achat).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Chanson = new SelectList(db.Chanson, "Id", "Titre", achat.Chanson);
+            return View(achat);
+        }
+
+        // GET: Achat/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Achat achat = db.Achat.Find(id);
+            if (achat == null)
+            {
+                return HttpNotFound();
+            }
+            return View(achat);
+        }
+
+        // POST: Achat/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Achat achat = db.Achat.Find(id);
+            db.Achat.Remove(achat);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
