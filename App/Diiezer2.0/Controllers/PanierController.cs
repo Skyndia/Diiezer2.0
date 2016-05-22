3db00f9646962650a502aac7c982abf677265696
Skyndia <balleuxbenjamin@gmail.com>
2016-05-22 16:38:08 +0200

    ajout des titres des pages + préparation de l'ajout d'un panier. Table panier ajoutée

---
+++ App/Diiezer2.0/Controllers/PanierController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Diiezer2._0.Controllers
+{
+    public class PanierController : Controller
+    {
+        // GET: Panier
+        public ActionResult MonPanier()
+        {
+            return View();
+        }
+    }
+}
